7a6ec8af778233ce1d283e8352b543a64246b7d7
lxiin00 <794164260@qq.com>
2019-11-21 23:58:52 +0800

    commit user home code

--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import loginView, logoutView
+from .views import loginView, logoutView, homeView
 
 
 urlpatterns = [
     path('login.html', loginView, name='login'),
     path('logout.html', logoutView, name='logout'),
+    path('home/<int:page>.html', homeView, name='home'),
 ]
--- user/views.py
+++ user/views.py
@@ -4,6 +4,9 @@ from django.db.models import Q
 from django.contrib.auth import login, logout
 from django.contrib.auth.hashers import check_password
 from .form import MyUserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import PageNotAnInteger, Paginator, EmptyPage
+from index.models import *
 
 
 def loginView(request):
@@ -21,19 +24,34 @@ def loginView(request):
                     tips = '密码错误'
             else:
                 tips = '用户不存在'
-    else:
-        user = MyUserCreationForm(request.POST)
-        if user.is_valid():
-            user.save()
-            tips = '注册成功'
         else:
-            if user.errors.get('username', ''):
-                tips = user.errors.get('username', '注册失败')
+            user = MyUserCreationForm(request.POST)
+            if user.is_valid():
+                user.save()
+                tips = '注册成功'
             else:
-                tips = user.errors.get('mobile', '注册失败')
+                if user.errors.get('username', ''):
+                    tips = user.errors.get('username', '注册失败')
+                else:
+                    tips = user.errors.get('mobile', '注册失败')
     return render(request, 'login.html', locals())
 
 
 def logoutView(request):
     logout(request)
     return redirect('/')
+
+
+
+@login_required(login_url='/user/login.html')
+def homeView(request, page):
+    search_song = Dynamic.objects.select_related('song').order_by('-dynamic_search').all()[:4]
+    song_info = request.session.get('play_list', [])
+    paginator = Paginator(song_info, 3)
+    try:
+        contacts = paginator.page(page)
+    except PageNotAnInteger:
+        contacts = paginator.page(1)
+    except EmptyPage:
+        contacts = paginator.page(paginator.num_pages)
+    return render(request, 'home.html', locals())
